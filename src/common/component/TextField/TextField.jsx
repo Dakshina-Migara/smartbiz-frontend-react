5c85e84af9b3abda7c7544afb713007e872778e0
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-03-05 10:49:38 +0530

    update the text field

--- src/common/component/TextField/TextField.jsx
+++ src/common/component/TextField/TextField.jsx
@@ -1,43 +1,37 @@
+import MuiTextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
 import './TextField.css'
 
-function TextField({
-    type = 'text',
-    placeholder = '',
-    value,
-    onChange,
-    icon,               // React node for the left icon
-    name,
-    id,
-    disabled = false,
-    required = false,
-    fullWidth = false,
-    className = '',
-    ...props
-}) {
-    const wrapperClasses = [
-        'textfield',
-        fullWidth ? 'textfield--full-width' : '',
-        disabled ? 'textfield--disabled' : '',
+export default function TextField({
+    type = 'text', placeholder = '', value, onChange, icon, name, id, disabled = false, required = false, fullWidth = false, className = '' }) {
+    const classNames = [
+        'smartbiz-textfield',
+        disabled ? 'smartbiz-textfield--disabled' : '',
         className
     ].filter(Boolean).join(' ')
 
     return (
-        <div className={wrapperClasses}>
-            {icon && <span className="textfield__icon">{icon}</span>}
-            <input
-                type={type}
-                className="textfield__input"
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                name={name}
-                id={id}
-                disabled={disabled}
-                required={required}
-                {...props}
-            />
-        </div>
+        <MuiTextField
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            name={name}
+            id={id}
+            disabled={disabled}
+            required={required}
+            fullWidth={fullWidth}
+            variant="outlined"
+            className={classNames}
+            slotProps={{
+                input: {
+                    startAdornment: icon ? (
+                        <InputAdornment position="start">
+                            <span className="smartbiz-textfield__icon">{icon}</span>
+                        </InputAdornment>
+                    ) : null,
+                }
+            }}
+        />
     )
 }
-
-export default TextField
