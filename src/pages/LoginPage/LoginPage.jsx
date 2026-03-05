c3b2cdc7220d565fd8fc16aebc045eb05e525ec0
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-03-05 10:50:08 +0530

    update the login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,19 +1,9 @@
 import { useState } from 'react'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Button from '../../common/component/Button/Button'
 import TextField from '../../common/component/TextField/TextField'
 import './LoginPage.css'
-
-const UserIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22">
-        <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v1.2c0 .66.54 1.2 1.2 1.2h16.8c.66 0 1.2-.54 1.2-1.2v-1.2c0-3.2-6.4-4.8-9.6-4.8z" />
-    </svg>
-)
-
-const LockIcon = () => (
-    < svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22" >
-        <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1s3.1 1.39 3.1 3.1v2z" />
-    </svg >
-)
 
 function LoginPage() {
     const [username, setUsername] = useState('')
@@ -32,8 +22,8 @@ function LoginPage() {
 
                 <div className="login-card__form">
                     <TextField
-                        icon={<UserIcon />}
-                        placeholder="Type Your User Name/Email"
+                        icon={<PersonOutlineIcon />}
+                        placeholder="Type Your Email"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                         fullWidth
@@ -41,7 +31,7 @@ function LoginPage() {
 
                     <TextField
                         type="password"
-                        icon={<LockIcon />}
+                        icon={<LockOutlinedIcon />}
                         placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
@@ -53,7 +43,7 @@ function LoginPage() {
                     </Button>
 
                     <Button variant="text">
-                        Forget Password/User Name?
+                        Forget Password/Email?
                     </Button>
                 </div>
 
@@ -65,4 +55,4 @@ function LoginPage() {
     )
 }
 
-export default LoginPage;
+export default LoginPage
