a911ec882126f3bb0a1e4f5ed874b4ba2d79dd51
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-03-05 11:54:56 +0530

    make using the react routing

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage/LoginPage'
+import RegisterPage from '../pages/RegisterPage/RegisterPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <LoginPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
