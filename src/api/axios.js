02c171b2b31a9ee83492b9b6b8542f57c718cbc8
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-05-24 10:04:37 +0530

    updates

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: 'http://65.0.205.142/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
